300a6d6d8740b6762d3d9f11d47947ccef7e0a17
JoanaClac <joanaclacoutinho@gmail.com>
2023-10-20 16:47:49 +0100

    Ex_Ficha2

--- FichasJava/src/FichaPratica02/Ex_03.java
+++ FichasJava/src/FichaPratica02/Ex_03.java
@@ -1,4 +1,20 @@
 package FichaPratica02;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Ex_03 {
+    public static void main(String[] args) {
+
+        // instanciar o Scanner (obrigatório para todos os exercícios)
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variaveis
+        double salario, impostos;
+
+        // Ler o salario
+        System.out.print ("Insira o salario: ")
+
+
+    }
 }
