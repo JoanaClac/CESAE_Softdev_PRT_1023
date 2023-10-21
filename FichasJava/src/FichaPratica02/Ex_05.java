fac844821cc2d4ca589a648e512a78aa577f1885
JoanaClac <joanaclacoutinho@gmail.com>
2023-10-21 11:24:10 +0100

    Teste_verde

---
+++ FichasJava/src/FichaPratica02/Ex_05.java
@@ -0,0 +1,15 @@
+package FichaPratica02;
+
+import java.util.Scanner;
+
+public class Ex_05 {
+    public static void main(String[] args) {
+
+        // instanciar o Scanner (obrigatório para todos os exercícios)
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variaveis
+
+
+    }
+}
